5ba9634d916638ce6e8e2a2dd4e13811e461270e
Bobby-Lai <bobby92307@gmail.com>
2018-12-20 03:23:35 +0800

    Line chatbot Practice

---
+++ config.js
@@ -0,0 +1,22 @@
+module.exports = {
+	line: {
+		channelSecret: 'a0c99d9fe4c569b370ea0d89bb1d335a',
+		accessToken: 'KkuYpUceZPTUtPE98pJPrNAd1D4vGCgU/Qa5ODl6UlAVOOkbO/wjH4O38kZgARtRVsZPt6Zm+FSQXmeid2gkZy42aj5q4GV9PMyZpcB/hQ9dW+0ueab76WbmuMbMC5JCbt5FIR/QmENnbswYDKbaYQdB04t89/1O/w1cDnyilFU='
+	},
+	messenger: {
+		accessToken: '',
+		appSecret: '',
+		verifyToken: '',
+	},
+	telegram: {
+		accessToken: '',
+	},
+	olami: {
+		appKey: 'be0538d7d05541ff9d9bcb271fe74177',
+		appSecret: '70bd7ce4dd394e6593e842722256cadc'
+	},
+	kkbox: {
+		id: '8285a8ddbabd78a17201471635380642',
+		secret: 'f392fd2968e3dd2182ef4a0b389dfcad'
+	}
+}
